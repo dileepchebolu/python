53a6296948cf162df0bf6669ebe327e64653771d
dileepchebolu <54228708+dileepchebolu@users.noreply.github.com>
2020-02-22 05:12:24 +0530

    added some files

---
+++ Examples/22.Modules_and_pip.py
@@ -0,0 +1,11 @@
+"""
+pip is a programme and it is by default coming with python3 # But in python2 we have to install it separately 
+
+generally, we use pip to install python modules and its like a programme manager. So we can use it to install, update and remove a python module.
+
+
+"""
+import os
+
+os.fsync()
+
